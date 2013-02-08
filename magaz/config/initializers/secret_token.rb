477bdc6fcdafcd9678e0c0fda8b739e5342726ac
SergXIIIth <sergey@makridenkov.com>
2013-02-08 09:25:11 +0100

    start

---
+++ magaz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magaz::Application.config.secret_token = 'a83079d42f4a7baa348604fdc64ed4da1980f759d82ac139b3ff44a45af69cd57ca4581f1dee8d3481eb169ac7aa47a2a9a0bce776bd56e5ccb18a68c28e213a'
