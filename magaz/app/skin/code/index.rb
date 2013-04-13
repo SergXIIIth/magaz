e3c86ba3498c2a9bcdafcff32c1fb7534d4d1b14
SergXIIIth <sergey@makridenkov.com>
2013-04-13 21:59:02 +0200

    Category porular

--- magaz/app/skin/code/index.rb
+++ magaz/app/skin/code/index.rb
@@ -5,8 +5,20 @@ class Index
     api.products.all.decorate
   end
 
+  def products_recomed
+    api.products.all.take(3)
+  end
+
+  def products_new
+    api.products.all.take(3)
+  end
+
   def categories
     api.categories.all.decorate
+  end
+
+  def categories_popular
+    api.categories.all.take(6)
   end
 
 end
